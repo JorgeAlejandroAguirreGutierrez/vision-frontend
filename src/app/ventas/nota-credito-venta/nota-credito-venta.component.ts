982207048e8ddcb9c24fbad8432909e147154720
Alejandro Aguirre <joaaguir@bancolombia.com.co>
2023-05-09 15:36:01 -0500

    mod

--- src/app/ventas/nota-credito-venta/nota-credito-venta.component.ts
+++ src/app/ventas/nota-credito-venta/nota-credito-venta.component.ts
@@ -184,7 +184,6 @@ export class NotaCreditoVentaComponent implements OnInit {
         this.seleccionCliente.patchValue(this.notaCreditoVenta.factura.cliente);
         this.facturaService.consultarPorCliente(this.notaCreditoVenta.factura.cliente.id).subscribe(
           res => {
-            this.seleccionFactura = new UntypedFormControl();
             this.facturas = res.resultado as Factura[]
           },
           err => Swal.fire({ icon: error_swal, title: error, text: err.error.codigo, footer: err.error.mensaje })
