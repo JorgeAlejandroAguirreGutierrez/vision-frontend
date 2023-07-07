80566a1d81589e16a6d1ff10f10ccfa63d64dbb2
Jorge Alejandro Aguirre Gutierrez <joaaguir@bancolombia.com.co>
2023-07-07 15:37:56 -0500

    mod

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/siice-frontend'),
+      dir: require('path').join(__dirname, './coverage/vision-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
