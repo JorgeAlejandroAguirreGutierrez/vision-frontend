5416bcb481f1715c9cc9436bd03d8add150b9675
Jorge Alejandro Aguirre Gutierrez <joaaguir@bancolombia.com.co>
2023-08-16 17:29:48 -0500

    mod

--- src/app/componentes/venta/factura/factura.component.ts
+++ src/app/componentes/venta/factura/factura.component.ts
@@ -373,7 +373,7 @@ export class FacturaComponent implements OnInit {
     this.dataSourceFactura.sort = this.sort;
   }
 
-  seleccion(factura: any) {
+  seleccionar(factura: any) {
     if (!this.clickedRowsFactura.has(factura)) {
       this.clickedRowsFactura.clear();
       this.clickedRowsFactura.add(factura);
